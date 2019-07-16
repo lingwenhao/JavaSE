6287724deeba7ea2b4759cf66cf4aac1ce4a97fa
ronin <lwh_ubuntu#163.com>
2019-07-17 00:23:52 +0800

    xxx

---
+++ oracle/src/main/java/com/dragonsoft/thread/LockTicketTest.java
@@ -0,0 +1,13 @@
+package com.dragonsoft.thread;
+
+public class LockTicketTest {
+    public static void main(String[] args) {
+        LockTicket lockTicket = new LockTicket();
+        Thread t1 = new Thread(lockTicket, "窗口1");
+        Thread t2 = new Thread(lockTicket, "窗口2");
+        Thread t3 = new Thread(lockTicket, "窗口3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
