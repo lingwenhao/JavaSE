677cecee9e83dc65e0408ca69d4bbe4c1bad11c6
ronin <lwh_ubuntu#163.com>
2019-07-24 22:57:23 +0800

    设计模式

--- designpattern/src/main/java/com/dragonsoft/structure/bridge/bridge_b/Client.java
+++ designpattern/src/main/java/com/dragonsoft/structure/bridge/bridge_b/Client.java
@@ -6,32 +6,30 @@ package com.dragonsoft.structure.bridge.bridge_b;
 public class Client {
     public static void main(String[] args) {
         //获取折叠式小米手机：样式+品牌
-            //多态
-        Brand ximi = new Ximi();
-        Phone ximiFolded = new FoldedPhone(ximi);
+        FoldedPhone ximiFolded = new FoldedPhone(new Ximi());
         ximiFolded.open();
         ximiFolded.call();
         ximiFolded.close();
+
         System.out.println("--------------------------------------");
         //获取折叠式Vivo手机：样式+品牌
-        Phone vivoFolded = new FoldedPhone(new Ximi());
+        FoldedPhone vivoFolded = new FoldedPhone(new Ximi());
         vivoFolded.open();
         vivoFolded.call();
         vivoFolded.close();
 
         System.out.println("=========================================");
         //获取直立式小米手机：样式+品牌
-//        Phone ximiUpRight = new UpRightPhone(new Ximi());
-//        ximiUpRight.open();
-//        ximiUpRight.call();
-//        ximiUpRight.close();
+        UpRightPhone ximiUpRight = new UpRightPhone(new Ximi());
+        ximiUpRight.open();
+        ximiUpRight.call();
+        ximiUpRight.close();
 
         System.out.println("--------------------------------------");
         //获取直立式Vivo手机：样式+品牌
-//        Phone vivoUpRight = new UpRightPhone(new Vivo());
-//        vivoUpRight.open();
-//        vivoUpRight.call();
-//        vivoUpRight.close();
-
+        UpRightPhone vivoUpRight = new UpRightPhone(new Vivo());
+        vivoUpRight.open();
+        vivoUpRight.call();
+        vivoUpRight.close();
     }
 }
---
+++ designpattern/src/main/java/com/dragonsoft/structure/decorator/decorator_b/Cholate.java
@@ -0,0 +1,12 @@
+package com.dragonsoft.structure.decorator.decorator_b;
+
+/**
+ * 具体的Decorator
+ */
+public class Cholate extends Decorator{
+    public Cholate(Drink drink) {
+        super(drink);
+        setDesc("巧克力");
+        setPrice(8.5);
+    }
+}
