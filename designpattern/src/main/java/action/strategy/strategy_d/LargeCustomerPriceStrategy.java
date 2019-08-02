2ae21ebcc1969fc7f6245e55708f8eab8423ff8b
lingwenhao <lwh_ubuntu@163.com>
2019-08-02 18:13:26 +0800

    策略模式模式

---
+++ designpattern/src/main/java/action/strategy/strategy_d/LargeCustomerPriceStrategy.java
@@ -0,0 +1,21 @@
+package action.strategy.strategy_d;
+
+/**
+ * @author ronin
+ * @version V1.0
+ * @desc    大客户:打8折
+ * @since 2019/8/2 15:12
+ */
+public class LargeCustomerPriceStrategy implements PriceStragegy {
+
+    /**
+     * 针对大客户的计算策略
+     *
+     * @param price 具体的价格
+     * @return
+     */
+    @Override
+    public double calcPrice(Double price) {
+        return price * 0.8;
+    }
+}
