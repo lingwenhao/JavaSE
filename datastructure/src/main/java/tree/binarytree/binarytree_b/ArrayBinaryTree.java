9f5efdc2e46cc237263385dc5715560e8b2b743c
ronin <lwh_ubuntu#163.com>
2019-08-23 00:36:49 +0800

    数据结构

---
+++ datastructure/src/main/java/tree/binarytree/binarytree_b/ArrayBinaryTree.java
@@ -0,0 +1,47 @@
+package tree.binarytree.binarytree_b;
+
+/**
+ * @author ronin
+ */
+public class ArrayBinaryTree {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        BinaryTree binaryTree = new BinaryTree(arr);
+        binaryTree.peOrder();
+    }
+
+}
+
+class BinaryTree{
+    /**
+     * 要遍历的数组
+     */
+    private int[] arr;
+
+    public BinaryTree(int[] arr) {
+        this.arr = arr;
+    }
+
+    public void peOrder(){
+        this.preOrder(0);
+    }
+    /**
+     * 前序遍历：根->左->右
+     * @param index 索引
+     */
+    private void preOrder(int index){
+        if(arr==null || arr.length == 0){
+            System.out.println("空数组......");
+        }
+        //输出当前元素
+        System.out.print(arr[index]+"\t");
+        //向左递归遍历
+        if((index*2+1) < arr.length){
+            preOrder(index*2+1);
+        }
+        //向右递归遍历
+        if((index*2+2) < arr.length){
+            preOrder(index*2+2);
+        }
+    }
+}
